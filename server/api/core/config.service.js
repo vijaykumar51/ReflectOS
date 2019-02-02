6db4396497f0364073a5172238d16946561f51aa
Vijay Kumar <vijay.kumar.em51@gmail.com>
2019-02-02 12:55:34 +0530

    Adding initial config service

---
+++ server/api/core/config.service.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const fs = require("fs");
+const glob = require("glob");
+
+const WIDGET_DIR = path.resolve(__dirname, "../widgets");
+const WIDGET_CONFIG = "widget-config.json";
+
+exports.getConfig = async () => {
+	let response = {};
+	response["widgets"] = {};
+	let widgetGlobalConfig = fs.readFileSync(
+		path.resolve(WIDGET_DIR, WIDGET_CONFIG),
+		"utf-8"
+	);
+
+	let configFiles = glob.sync("**/config.json", { cwd: WIDGET_DIR });
+	configFiles.forEach(file => {
+		let widgetConfig = JSON.parse(
+			fs.readFileSync(path.resolve(WIDGET_DIR, file), "utf-8")
+		);
+		response["widgets"][widgetConfig.widgetName] = widgetConfig;
+	});
+
+	response["widgetsParentConfg"] = JSON.parse(widgetGlobalConfig);
+	response["files"] = configFiles;
+	return response;
+};
+
+getWidgetConfig = () => {};
--- server/http-server.js
+++ server/http-server.js
@@ -1,6 +1,7 @@
 require("module-alias/register");
 var express = require("express");
 var routes = require("@widgets/weather/weather.route");
+var configService = require("./api/core/config.service");
 
 var app = express();
 const port = 9090;
@@ -11,6 +12,13 @@ app.use("/", routes);
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/config", (req, res) => {
+	let data = configService.getConfig().then(data => {
+		res.json(data);
+	});
+	// console.log(data);
 });
 
 app.listen(port, () => {
