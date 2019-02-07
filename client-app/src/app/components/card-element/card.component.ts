b55d1ef15294bb2769717fb26bbbe98f1541393a
Vijay Kumar <vijay.kumar.em51@gmail.com>
2019-02-07 20:08:17 +0530

    Removing angular code

--- client-app/src/app/components/card-element/card.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-
-@Component({
-	selector: "app-card",
-	templateUrl: "./card.component.html",
-	encapsulation: ViewEncapsulation.Native
-})
-export class CardComponent {}
